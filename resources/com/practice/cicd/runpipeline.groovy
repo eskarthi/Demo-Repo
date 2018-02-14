19c9136ffad347b5babd14b4a0b4a5fab2128597
eskarthi <eskarthi@me.com>
2018-02-14 17:00:36 +0800

    moved source from untrusted to parent folder

---
+++ resources/com/practice/cicd/runpipeline.groovy
@@ -0,0 +1,27 @@
+def call () {
+	pipeline {
+		agent any
+		environment { // global settings
+				mvnHome = tool 'Maven3.5.2'
+			}	
+		
+		stages {
+			 
+			stage('Build') {
+				steps {
+					bat(/"${mvnHome}\bin\mvn" -B -DskipTests clean package/)
+				}
+			}
+			stage('Test') {
+				steps {
+					bat(/"${mvnHome}\bin\mvn" test/)
+				}
+				post {
+					always {
+					   junit '**/target/surefire-reports/*.xml'
+					}
+				}
+			}
+		}
+	}
+}
