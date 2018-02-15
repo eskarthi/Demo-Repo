4a40e06a32f69e835cfb50ac29ceafc33e1bc6af
eskarthi <eskarthi@me.com>
2018-02-15 23:28:16 +0800

    modified runpipeline - removed node

--- vars/runpipeline.groovy
+++ vars/runpipeline.groovy
@@ -1,12 +1,11 @@
 def call (String name = 'human') {
 
-		node {
         stage('Pre Build') {
           steps {
             echo "The build number is ...."
 			echo "Triggered by, ${name}."
           }
         }
-      }
+      
     
 }
