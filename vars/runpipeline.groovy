5d71f77f55f0d60f105ce4d7d772fc75f43a3807
eskarthi <eskarthi@me.com>
2018-02-16 00:16:54 +0800

    modified runpipeline - created def variable for mvnhome

--- vars/runpipeline.groovy
+++ vars/runpipeline.groovy
@@ -1,21 +1,31 @@
 def call (String name = 'human') {
 
     node {
-	
 		def mvnHome
 		stage('Preparation') { // for display purposes
-		// Get some code from a GitHub repository
-		git 'https://github.com/eskarthi/simple-java-maven-app.git'
-		// Get the Maven tool.
-		// ** NOTE: This maven tool configured
-		// **       in the global configuration.           
-		mvnHome = tool 'Maven3.5.2'
+	        echo "The build number is ...."
+			echo "Triggered by, ${name}."
+			// Get some code from a GitHub repository
+			git 'https://github.com/eskarthi/simple-java-maven-app.git'
+			// Get the Maven tool.
+			// ** NOTE: This maven tool configured
+			// **       in the global configuration.           
+			mvnHome = tool 'Maven3.5.2'
 		}
         
 		stage ('Build') {
-            echo "The build number is ...."
-			echo "Triggered by, ${name}."
-			bat(/"${mvnHome}\bin\mvn" -B -DskipTests clean package/)
+    		bat(/"${mvnHome}\bin\mvn" -B -DskipTests clean package/)
           }
+		
+		stage('Test') {
+				steps {
+					bat(/"${mvnHome}\bin\mvn" test/)
+				}
+				post {
+					always {
+					   junit '**/target/surefire-reports/*.xml'
+					}
+				}
+			}
         }
 }
