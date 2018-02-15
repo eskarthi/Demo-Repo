5ff8c0c08f3d68f3e22c856349f59a57edad3ab1
eskarthi <eskarthi@me.com>
2018-02-16 00:07:53 +0800

    modified runpipeline - created def variable for mvnhome

--- vars/runpipeline.groovy
+++ vars/runpipeline.groovy
@@ -1,10 +1,21 @@
 def call (String name = 'human') {
 
     node {
-          stage ('Build') {
+	
+		def mvnHome
+		stage('Preparation') { // for display purposes
+		// Get some code from a GitHub repository
+		git 'https://github.com/jglick/simple-maven-project-with-tests.git'
+		// Get the Maven tool.
+		// ** NOTE: This maven tool configured
+		// **       in the global configuration.           
+		mvnHome = tool 'Maven3.5.2'
+		}
+        
+		stage ('Build') {
             echo "The build number is ...."
 			echo "Triggered by, ${name}."
-			bat(/"C:\Tools\apache-maven-3.5.2\bin\mvn" -B -DskipTests clean package/)
+			bat(/"${mvnHome}\bin\mvn" -B -DskipTests clean package/)
           }
         }
 }
