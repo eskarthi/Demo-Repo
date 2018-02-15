3e0a4d0ebe5102c2be2d5ee3a2bdd3a98b7c905e
eskarthi <eskarthi@me.com>
2018-02-15 23:17:47 +0800

    modified runpipeline - removed environment

--- vars/runpipeline.groovy
+++ vars/runpipeline.groovy
@@ -1,9 +1,6 @@
 def call (String name = 'human') {
 	pipeline {
 		agent any
-		environment { // global settings
-				mvnHome = tool 'Maven3.5.2'
-		}	
 		 // Scripted Pipeline
 		stages {
         stage('Even Stage') {
