f149cd5c51c7777d7e2964a0c9f15049e0cf68c7
eskarthi <eskarthi@me.com>
2018-02-20 00:15:44 +0800

    removed steps and post always

--- vars/runpipeline.groovy
+++ vars/runpipeline.groovy
@@ -6,46 +6,42 @@ def call(body) {
 	body()
 
 
-		node {
-			def mvnHome
-			// Clean workspace before doing anything
-			//deleteDir()
+	node {
+		def mvnHome
+		// Clean workspace before doing anything
+		//deleteDir()
 
-			try {
-				stage ('Preparation') {
-					//checkout scm
-					echo "Triggered by, ${config.user}"
-					// Get some code from a GitHub repository
-					git 'https://github.com/eskarthi/simple-java-maven-app.git'
+		try {
+			stage ('Preparation') {
+				//checkout scm
+				echo "Triggered by, ${config.user}"
+				// Get some code from a GitHub repository
+				git 'https://github.com/eskarthi/simple-java-maven-app.git'
 
-					mvnHome = tool 'Maven3.5.2'
-				}
-				
-				stage ('Build') {
-					echo "building ${config.projectName} ..."
-					bat(/"${mvnHome}\bin\mvn" -B -DskipTests clean package/)
-				}
-
-				stage ('Tests') {
-					steps {
-						echo "shell scripts to run unit tests..."
-						bat(/"${mvnHome}\bin\mvn" test/)
-					}
-					post {
-						always {
-							echo "Generate test report..."
-							junit '**/target/surefire-reports/*.xml'
-						}
-					}
-				}
-
-				stage ('Deploy') { echo "deploying to server ${config.serverDomain}..." }
-
-			} catch (err) {
-				currentBuild.result = 'FAILED'
-				throw err
+				mvnHome = tool 'Maven3.5.2'
 			}
+
+			stage ('Build') {
+				echo "building ${config.projectName} ..."
+				bat(/"${mvnHome}\bin\mvn" -B -DskipTests clean package/)
+			}
+
+			stage ('Tests') {
+				echo "shell scripts to run unit tests..."
+				bat(/"${mvnHome}\bin\mvn" test/)
+			}
+			stage ('Test Result') {
+				echo "Generate test report..."
+				junit '**/target/surefire-reports/*.xml'
+			}
+
+			stage ('Deploy') {  echo "deploying to server ${config.serverDomain}..."  }
+
+		} catch (err) {
+			currentBuild.result = 'FAILED'
+			throw err
 		}
-	
+	}
+
 }
 
