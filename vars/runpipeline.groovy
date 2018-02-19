42aa4aa5bf750716eaee6f871469e97bcd925ecb
eskarthi <eskarthi@me.com>
2018-02-19 23:54:24 +0800

    removed parallel execution and made it as sequential

--- vars/runpipeline.groovy
+++ vars/runpipeline.groovy
@@ -1,53 +1,53 @@
 def call(body) {
-	
-			def config = [:]
-			body.resolveStrategy = Closure.DELEGATE_FIRST
-			body.delegate = config
-			body()
-	
-			pipeline {
-				agent any
-			
-			node {
-				def mvnHome
-				// Clean workspace before doing anything
-				//deleteDir()
-	
-				try {
-					stage ('Preparation') {
-						//checkout scm
-						echo "Triggered by, ${config.user}"
-						// Get some code from a GitHub repository
-						git 'https://github.com/eskarthi/simple-java-maven-app.git'
-	
-						mvnHome = tool 'Maven3.5.2'
+
+	def config = [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config
+	body()
+
+	pipeline {
+		agent any
+
+		node {
+			def mvnHome
+			// Clean workspace before doing anything
+			//deleteDir()
+
+			try {
+				stage ('Preparation') {
+					//checkout scm
+					echo "Triggered by, ${config.user}"
+					// Get some code from a GitHub repository
+					git 'https://github.com/eskarthi/simple-java-maven-app.git'
+
+					mvnHome = tool 'Maven3.5.2'
+				}
+				
+				stage ('Build') {
+					echo "building ${config.projectName} ..."
+					bat(/"${mvnHome}\bin\mvn" -B -DskipTests clean package/)
+				}
+
+				stage ('Tests') {
+					steps {
+						echo "shell scripts to run unit tests..."
+						bat(/"${mvnHome}\bin\mvn" test/)
 					}
-					stage ('Build') {
-						echo "building ${config.projectName} ..."
-						bat(/"${mvnHome}\bin\mvn" -B -DskipTests clean package/)
-					}
-					stage ('Tests') {
-						parallel 'static': {
-							echo "shell scripts to run static tests..."
-						},
-						'unit': {
-							echo "shell scripts to run unit tests..."
-							bat(/"${mvnHome}\bin\mvn" test/)
-							
-						},
-						'close': {
+					post {
+						always {
 							echo "Generate test report..."
 							junit '**/target/surefire-reports/*.xml'
 						}
 					}
-					stage ('Deploy') {
-						echo "deploying to server ${config.serverDomain}..."
-					}
-				} catch (err) {
-					currentBuild.result = 'FAILED'
-					throw err
 				}
-			}
+
+				stage ('Deploy') { echo "deploying to server ${config.serverDomain}..." }
+
+			} catch (err) {
+				currentBuild.result = 'FAILED'
+				throw err
 			}
 		}
+	}
+}
 
